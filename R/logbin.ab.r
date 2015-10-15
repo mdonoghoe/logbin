d76c0eabad0b02160d356b9c7e9995b5ad678bcf
Mark Donoghoe <markdonoghoe@gmail.com>
2015-10-16 09:07:08 +1100

    Set starting value if none supplied to adaptive barrier

--- R/logbin.ab.r
+++ R/logbin.ab.r
@@ -15,16 +15,26 @@ logbin.ab <- function(mt, mf, Y, offset, mono, start, control, control.method, w
   eval(fam$initialize)
   y1 <- round(n * y)
   
-  theta.start <- if (!is.null(start)) {
+  if (!is.null(start)) {
     if (length(start) != nvars)
       stop(gettextf("length of 'start' should equal %d and correspond to initial coefs for %s",
                     nvars, paste(deparse(xnames), collapse = ", ")),
            domain = NA)
     else if (any(x %*% start + offset >= 0))
       stop("'start' is on or outside the boundary of the parameter space", domain = NA)
-    else start
+    else
+      theta.start <- start
   } else {
-    # Calculate starting value that will always be inside the parameter space?
+    allref <- logbin.allref(mt, mf, "cem", mono, NULL)
+    if (length(allref$allref) == 0)
+      theta.start <- log(mean(y)) - 2 * control$bound.tol
+    else {
+      design.numref <- sapply(allref$allref, length)
+      design.all <- expand.grid(lapply(design.numref, seq_len))
+      start.np <- rep((log(mean(y)) - 2 * control$bound.tol) / nvars, nvars)
+      reparam <- logbin.reparameterise(start.np, mt, mf, "cem", allref$allref, allref$monotonic, design.all[1, ])
+      theta.start <- reparam$coefs
+    }
   }
   
   negll <- function(theta, y, n, x, o) {
