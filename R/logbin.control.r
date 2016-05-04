c5d126422fcc13d5f97f82e9fb6e79ada2d917c7
Mark Donoghoe <markdonoghoe@gmail.com>
2016-05-04 13:49:40 +1000

    Warning if epsilon >= bound.tol

--- R/logbin.control.r
+++ R/logbin.control.r
@@ -4,6 +4,8 @@ logbin.control <- function (bound.tol = 1e-06, epsilon = 1e-08, maxit = 10000, t
         stop("value of 'bound.tol' must be > 0")
     if (!is.numeric(epsilon) || epsilon <= 0) 
         stop("value of 'epsilon' must be > 0")
+    if (epsilon >= bound.tol)
+      warning("'epsilon' should be smaller than 'bound.tol'", call. = FALSE)
     if (!is.numeric(maxit) || maxit <= 0) 
         stop("maximum number of iterations must be > 0")
     list(bound.tol = bound.tol, epsilon = epsilon, maxit = maxit, trace = trace)
